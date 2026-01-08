9e8becfe6647f8447f3e35654f1578ee7c572a0a
jellyfishing2346 <108194702+jellyfishing2346@users.noreply.github.com>
2026-01-07 20:33:05 -0500

    Adjusting path errors

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,8 +1,9 @@
+
 import '@testing-library/jest-dom';
-jest.mock('react-leaflet');
-jest.mock('leaflet');
 import { render, screen } from '@testing-library/react';
 import App from './App';
+jest.mock('react-leaflet');
+jest.mock('leaflet');
 
 test('renders welcome message', () => {
   render(<App />);
